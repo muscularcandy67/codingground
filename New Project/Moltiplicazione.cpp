6737809dddb606fa7fca3d56e48dde1b27613bb0
muscularcandy67 <muscularcandy67@gmail.com>
2015-05-07 11:49:01 +0200

    Update Moltiplicazione.cpp

--- New Project/Moltiplicazione.cpp
+++ New Project/Moltiplicazione.cpp
@@ -1,11 +1,18 @@
-#include <iostream>
-
+#include<iostream>
+#include<math.h>
 using namespace std;
 
-int main()
-{
-   cout << "Hello World" << endl; 
-   
-   return 0;
-}
-
+int main (){
+    int A,B,T;
+    cout<<"Inserisci il numero da moltiplicare"<<endl;
+    cin>>A;
+    cout<<"Inserisci il numero per cui moltiplicare"<<endl;
+    cin>>B;
+    T=0;
+    do{
+          T=T+A;
+          B=B-1;
+          }while(B>0);
+          cout<<"Il risultato e' "<<T<<endl;
+          system ("pause");
+          }
